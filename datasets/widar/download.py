42cb926cfbdf8704fbb2b9617188049a02435bdc
Samiul Alam <samiul272@gmail.com>
2023-04-12 18:44:08 +0600

    updated widar

--- datasets/widar/download.py
+++ datasets/widar/download.py
@@ -1,65 +1,37 @@
-import io
 import os
 import zipfile
 
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+import gdown
 
-# Define the Google Drive folder ID
-# Manually download from https://drive.google.com/drive/folders/1R0R8SlVbLI1iUFQCzh_mH90H_4CW2iwt
-FOLDER_ID = "1R0R8SlVbLI1iUFQCzh_mH90H_4CW2iwt"
+# Define the shared Google Drive file URL
+FILE_URL = "https://drive.google.com/uc?id=1tnvLL8Wg0p9f3qJzyIYvevFZkWtV0AAh"
 
 # Define the directory where you want to save the dataset
 SAVE_DIR = "./"
 
-# Add your service account file path
-SERVICE_ACCOUNT_FILE = "path/to/your-service-account-file.json"
-
-
-# Function to create Google Drive API client
-def create_google_drive_client(service_account_file):
-    credentials = service_account.Credentials.from_service_account_file(service_account_file)
-    try:
-        service = build("drive", "v3", credentials=credentials)
-        return service
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-        return None
-
 
 # Function to download the file from Google Drive
-def download_file_from_google_drive(service, file_id, save_dir):
+def download_file_from_google_drive(url, save_dir):
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
-    request = service.files().get_media(fileId=file_id)
-    file = io.BytesIO(request.execute())
-    zip_ref = zipfile.ZipFile(file, "r")
-    zip_ref.extractall(save_dir)
+    file_path = os.path.join(save_dir, "WidarData.zip")
+    gdown.download(url, file_path, quiet=False)
+
+    return file_path
+
+
+# Function to extract the dataset
+def extract_file(file_path, save_dir):
+    with zipfile.ZipFile(file_path, "r") as zip_ref:
+        zip_ref.extractall(save_dir)
     print(f"Extracted dataset to {save_dir}")
-
-
-# Function to list all files in the specified Google Drive folder
-def list_files_in_google_drive_folder(service, folder_id):
-    query = f"'{folder_id}' in parents"
-    results = service.files().list(q=query, fields="nextPageToken, files(id, name)").execute()
-    items = results.get("files", [])
-
-    return items
 
 
 # Main function to download and extract the WidarData.zip file
 def main():
-    service = create_google_drive_client(SERVICE_ACCOUNT_FILE)
-    if service is not None:
-        files = list_files_in_google_drive_folder(service, FOLDER_ID)
-        for file in files:
-            if file["name"] == "WidarData.zip":
-                download_file_from_google_drive(service, file["id"], SAVE_DIR)
-                break
-        else:
-            print("WidarData.zip not found in the specified folder.")
+    file_path = download_file_from_google_drive(FILE_URL, SAVE_DIR)
+    extract_file(file_path, SAVE_DIR)
 
 
 if __name__ == "__main__":
