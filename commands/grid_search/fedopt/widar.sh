675892b2b9d7b8ac525c309d6978ba233ec340df
Samiul Alam <samiul272@gmail.com>
2023-07-03 13:04:41 -0400

    updated base_fl.py

--- commands/grid_search/fedopt/widar.sh
+++ commands/grid_search/fedopt/widar.sh
@@ -1,11 +1,11 @@
-lrs=(0.01 0.001 0.0001)
+lrs=(0.1 0.01 0.001 0.0001)
 for client_lr in "${lrs[@]}"
 do
   for server_lr in "${lrs[@]}"
   do
     ### NIID-0.1 Adam 10%-30%
-    num_gpus=1 num_trainers_per_gpu=4 python distributed_main.py main  --dataset_name widar --model Widar_ResNet18  --client_num_in_total 40 --client_num_per_round 4 --partition_type dirichlet --alpha 0.1 --lr $client_lr --server_optimizer adam --server_lr $server_lr --test_frequency 5 --comm_round 300 --batch_size 32 --analysis baseline --trainer BaseTrainer --amp --watch_metric accuracy
+    num_gpus=1 num_trainers_per_gpu=4 python distributed_main.py main  --dataset_name widar --model Widar_ResNet18  --client_num_in_total 40 --client_num_per_round 4 --partition_type dirichlet --alpha 0.1 --lr $client_lr --server_optimizer adam --server_lr $server_lr --test_frequency 5 --comm_round 1000 --batch_size 8 --analysis baseline --trainer BaseTrainer --amp --watch_metric accuracy
     ### NIID-0.5 Adam 10%-30%
-    num_gpus=1 num_trainers_per_gpu=4 python distributed_main.py main  --dataset_name widar --model Widar_ResNet18  --client_num_in_total 40 --client_num_per_round 4 --partition_type dirichlet --alpha 0.5 --lr $client_lr --server_optimizer adam --server_lr $server_lr --test_frequency 5 --comm_round 300 --batch_size 32 --analysis baseline --trainer BaseTrainer --amp --watch_metric accuracy
+    num_gpus=1 num_trainers_per_gpu=4 python distributed_main.py main  --dataset_name widar --model Widar_ResNet18  --client_num_in_total 40 --client_num_per_round 4 --partition_type dirichlet --alpha 0.5 --lr $client_lr --server_optimizer adam --server_lr $server_lr --test_frequency 5 --comm_round 1000 --batch_size 8 --analysis baseline --trainer BaseTrainer --amp --watch_metric accuracy
   done
 done
