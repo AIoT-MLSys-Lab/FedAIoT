bd7bdcac8a8d66feb11fc0cac3116ce1dfeb5389
Samiul Alam <samiul272@gmail.com>
2023-07-09 21:02:24 -0400

    fixed label noise command bugs

--- commands/label_error/ut_har.sh
+++ commands/label_error/ut_har.sh
@@ -4,5 +4,6 @@
 client_lr=0.0001
 for seed in {1..3}
 do
-    seed=$seed num_gpus=1 num_trainers_per_gpu=2 python distributed_main.py main  --dataset_name ut_har --model UT_HAR_ResNet18  --client_num_in_total 20 --client_num_per_round 2 --partition_type dirichlet --alpha 0.1 --lr $client_lr --server_optimizer sgd --server_lr 1 --test_frequency 5 --comm_round 1200 --batch_size 32 --analysis label_noise --trainer BaseTrainer --amp --watch_metric accuracy
+    seed=$seed num_gpus=1 num_trainers_per_gpu=2 python distributed_main.py main  --dataset_name ut_har --model UT_HAR_ResNet18  --client_num_in_total 20 --client_num_per_round 2 --partition_type dirichlet --alpha 0.1 --lr $client_lr --server_optimizer sgd --server_lr 1 --test_frequency 5 --comm_round 1200 --batch_size 32 --analysis label_noise-0.1-0.0 --trainer BaseTrainer --amp --watch_metric accuracy
+    seed=$seed num_gpus=1 num_trainers_per_gpu=2 python distributed_main.py main  --dataset_name ut_har --model UT_HAR_ResNet18  --client_num_in_total 20 --client_num_per_round 2 --partition_type dirichlet --alpha 0.1 --lr $client_lr --server_optimizer sgd --server_lr 1 --test_frequency 5 --comm_round 1200 --batch_size 32 --analysis label_noise-0.3-0.0 --trainer BaseTrainer --amp --watch_metric accuracy
 done
